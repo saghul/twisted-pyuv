effd269a3623da28540d397698f2002bd2ebcfdb
saghul <saghul@gmail.com>
2012-06-10 11:24:52 +0200

    Raised version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name             = 'twisted-pyuv',
-    version          = '0.1.0',
+    version          = '0.2.0',
     url              = 'https://github.com/saghul/twisted-pyuv',
     author           = 'Saúl Ibarra Corretgé',
     author_email     = 'saghul@gmail.com',
